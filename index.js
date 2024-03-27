401a9806230f63e5ca4cf0be4eed1229aa61567a
omorut <hillarydavisomorut@gmail.com>
2024-03-28 02:01:03 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.ceil(Math.random() * 100) + 1;
+
+let num3 = 20;
+let num4 = 8;
+
+const mod = num3 % num4;
+
+let max = Math.max(5,10,15,20);
+
+console.log(max); 
